c098df438e019889476bb9f2aa83ef34871ebe50
Md. Maklachur Rahman <50495850+maklachur@users.noreply.github.com>
2020-08-28 01:11:38 +0900

    Update Image classification by modifying pretrained model.py
    showed the usage of more pre-trained models

--- Classification/Image classification by modifying pretrained model.py
+++ Classification/Image classification by modifying pretrained model.py
@@ -44,22 +44,30 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           num_workers=num_workers)
 
 #import pretrained model
-model=torchvision.models.resnet50(pretrained=True)
+#model = torchvision.models.resnet50(pretrained=True)  #uncomment this line for using resnet50
+model = torchvision.models.vgg16(pretrained=True)  #uncomment this line for using vgg16
+#model = torchvision.models.densenet121(pretrained=True)  #uncomment this line for using densenet121
 
 #check inside the model architecture
 #print(model)
 
-input_channel = model.fc.in_features
+#input_channel = model.classifier.in_features  #uncomment this line for  using resnet50
+input_channel = model.classifier[0].in_features  #uncomment this line for using vgg16
+#input_channel = model.classifier.in_features  #uncomment this line for using densenet121
+
+#to finetune the model, set requires_grad = False
+#OR, comment these below two lines to train entire model
 for param in model.parameters():
     param.requires_grad = False
  
 #We are going to change only in the fc layer
-model.fc = nn.Sequential(nn.Linear(input_channel, 128),
+model.classifier = nn.Sequential(nn.Linear(input_channel, 128),
                          nn.ReLU(),
                          nn.Linear(128, num_classes))
 
 
 model = model.to(device)
+
 
 # define loss and gradient
 criterion = nn.CrossEntropyLoss()
